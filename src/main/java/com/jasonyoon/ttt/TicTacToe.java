4dd5f67669f410b9d181b9bb1645c7536d020790
U-LYV\jason.yoon <jason.yoon@USHOLMEG-L-LN.LYV.LiveNation.com>
2015-07-06 15:53:45 -0700

    fully functioning

--- src/main/java/com/jasonyoon/ttt/TicTacToe.java
+++ src/main/java/com/jasonyoon/ttt/TicTacToe.java
@@ -2,19 +2,31 @@ package com.jasonyoon.ttt;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class TicTacToe {
 
     private char[][] board;
+    private char winner = '?';
+    private int turnsPlayed = 0;
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException{
+        TicTacToe t = new TicTacToe();
+        t.playGame(System.in);
+    }
 
+    public char[][] getBoard(){
+        return board;
+    }
+
+    public char getWinner() {
+        return winner;
     }
 
     public TicTacToe() {
-        this.board = new char[3][3];
+        board = new char[3][3];
         //logically unnecessary, but helps board interface stay "in place" by
         //initializing as ' ' rather than leaving it as a null character
         for(char[] row : board) {
@@ -29,23 +41,61 @@ public class TicTacToe {
         System.out.print("\n");
     }
 
-    public void makeMove(char player, int row, int col) {
+    public boolean makeMove(char player, int row, int col) {
+        //System.out.println("row:" + row + "col:" + col);
+        if(row < 0 || 2 < row || col < 0 || 2 < col) {
+            System.out.println("Row and column must be between 0 and 2 inclusive.");
+            return false;
+        } else if(board[row][col] != ' ') {
+            System.out.print(row + ", " + col + " is already filled.\n");
+            return false;
+        }
+
         board[row][col] = player;
+
+        //check if this move finishes the game
+        if((board[row][0] == player && board[row][1] == player && board[row][2] == player)
+            || (board[0][col] == player && board[1][col] == player && board[2][col] == player)
+            || (board[0][col] == player && board[1][col] == player && board[2][col] == player)
+            || (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            || (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+            winner = player;
+        }
+        return true;
     }
 
-    public void playGame(){
-        boolean playersTurn = true;
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                if (playersTurn) {
-                    makeMove('X', i, j);
-                } else {
-                    makeMove('O', i, j );
+    public char[][] playGame(InputStream is) throws IOException{
+        System.out.println("Please specify the row and column in the following format: 'ROW' 'COL'\nRow and column must be between 0 and 2 inclusive.\nExample: 1 2\n");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        this.displayBoard();
+
+        String input;
+        char player = 'X';
+        int row, col;
+
+        while(turnsPlayed < 9){
+            if (player == 'X') {
+                System.out.println("Player X's turn: What's your move?");
+            } else {
+                System.out.println("Player O's turn: What's your move?");
+            }
+
+            if((input = br.readLine()) != null && input.length() >= 3) {
+                row = Character.getNumericValue(input.charAt(0));
+                col = Character.getNumericValue(input.charAt(2));
+                if(makeMove(player, row, col)) {
+                    displayBoard();
+                    if(winner != '?') {
+                        break;
+                    }
+                    player = (player == 'X') ? 'O' : 'X';
+                    turnsPlayed++;
                 }
-                playersTurn = !playersTurn;
-                displayBoard();
+            } else {
+                System.out.println("Please specify row and column in the following format: 'ROW' 'COL'");
             }
         }
+        System.out.println(winner + " won!");
+        return this.board;
     }
-
 }
--- src/test/java/com/jasonyoon/ttt/TicTacToeTest.java
+++ src/test/java/com/jasonyoon/ttt/TicTacToeTest.java
@@ -4,41 +4,64 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.Before;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by jason.yoon on 7/2/2015.
  */
+
 public class TicTacToeTest {
     TicTacToe ttt;
     PrintStream stdout;
+    InputStream stdin;
     ByteArrayOutputStream consoleOutput;
+
 
     @Before
     public void setup() {
         ttt = new TicTacToe();
-        //redirect output so we can compare
+        stdin = System.in;
         stdout = System.out;
-        consoleOutput = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(consoleOutput));
     }
 
     @After
     public void cleanup() {
         System.setOut(stdout);
+        System.setIn(stdin);
     }
 
     @Test
     public void displaysEmptyBoard() {
+        //redirect output so we can compare
+        consoleOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(consoleOutput));
+
         String expectedOutput = "|   |   |   |\n|   |   |   |\n|   |   |   |\n\n";
         ttt.displayBoard();
         assertEquals(expectedOutput, consoleOutput.toString());
     }
 
     @Test
+    public void makeMoveWorks() {
+        char[][] expected = {{' ', 'X', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        ttt.makeMove('X', 0, 1);
+        assertTrue(Arrays.deepEquals(expected, ttt.getBoard()));
+        expected[0][0] = 'O';
+        ttt.makeMove('O', 0, 0);
+        assertTrue(Arrays.deepEquals(expected, ttt.getBoard()));
+    }
+
+    @Test
     public void displaysCompletedBoard() {
+        //redirect output so we can compare
+        consoleOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(consoleOutput));
+
         String expectedOutput = "| X | O | X |\n| O | X | O |\n| X | O | X |\n\n";
         fillBoard();
         ttt.displayBoard();
@@ -58,22 +81,63 @@ public class TicTacToeTest {
     }
 
     @Test
-    public void playsAutomaticallyAlternating() {
-        ttt.playGame();
-        String expectedOutput =
-                "| X |   |   |\n|   |   |   |\n|   |   |   |\n\n"
-                + "| X | O |   |\n|   |   |   |\n|   |   |   |\n\n"
-                + "| X | O | X |\n|   |   |   |\n|   |   |   |\n\n"
-                + "| X | O | X |\n| O |   |   |\n|   |   |   |\n\n"
-                + "| X | O | X |\n| O | X |   |\n|   |   |   |\n\n"
-                + "| X | O | X |\n| O | X | O |\n|   |   |   |\n\n"
-                + "| X | O | X |\n| O | X | O |\n| X |   |   |\n\n"
-                + "| X | O | X |\n| O | X | O |\n| X | O |   |\n\n"
-                + "| X | O | X |\n| O | X | O |\n| X | O | X |\n\n";
-        assertEquals(expectedOutput, consoleOutput.toString());
+    public void playsAutomaticallyAlternating() throws IOException{
+        //redirect output so we can compare
+        consoleOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(consoleOutput));
+
+        String userInputString = "0 0\n0 1\n0 2\n1 0\n1 1\n1 2\n2 0\n2 1\n2 2\n";
+        InputStream is = new ByteArrayInputStream(userInputString.getBytes());
+        char[][] expected = {{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'X', 'O', 'X'}};
+        assertTrue(Arrays.deepEquals(expected, ttt.playGame(is)));
     }
 
+    @Test
+    public void takesMockPlayerInput() throws IOException{
+        String userInputString = "0 0\n1 2\n0 1\n2 0\n0 2\n2 1\n1 0\n2 2\n1 1\n";
+        InputStream is = new ByteArrayInputStream(userInputString.getBytes());
+        char[][] expected = {{'X', 'X', 'X'}, {'X', 'X', 'O'}, {'O', 'O', 'O'}};
+        assertTrue(Arrays.deepEquals(expected, ttt.playGame(is)));
+    }
 
+    @Test
+    public void ifAlreadyFilledOutputMessage() {
+        consoleOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(consoleOutput));
+        ttt.makeMove('X', 1, 1);
+        ttt.makeMove('O', 1, 1);
+        assertEquals("1, 1 is already filled.\n", consoleOutput.toString());
+    }
 
+    @Test
+    public void threeInSingleRowWins() {
+        ttt.makeMove('X', 0, 0);
+        ttt.makeMove('X', 0, 1);
+        ttt.makeMove('X', 0, 2);
+        assertEquals('X', ttt.getWinner());
+    }
 
+    @Test
+    public void threeInSingleColumnWins() {
+        ttt.makeMove('Y', 0, 0);
+        ttt.makeMove('Y', 1, 0);
+        ttt.makeMove('Y', 2, 0);
+        assertEquals('Y', ttt.getWinner());
+    }
+
+    @Test
+    public void threeInSingleDiagonalWins1() {
+        ttt.makeMove('Y', 0, 0);
+        ttt.makeMove('Y', 1, 1);
+        ttt.makeMove('Y', 2, 2);
+        assertEquals('Y', ttt.getWinner());
+    }
+
+    @Test
+    public void threeInSingleDiagonalWins2() {
+        ttt.makeMove('Y', 0, 2);
+        ttt.makeMove('Y', 1, 1);
+        ttt.makeMove('Y', 2, 0);
+        assertEquals('Y', ttt.getWinner());
+    }
 }
