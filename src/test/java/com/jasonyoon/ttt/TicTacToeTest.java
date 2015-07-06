34aa8de6a06733cb7da6dd3dc64e34735f6a8098
U-LYV\jason.yoon <jason.yoon@USHOLMEG-L-LN.LYV.LiveNation.com>
2015-07-06 10:35:50 -0700

    plays automatically, tests output by redirecting stdout

--- src/test/java/com/jasonyoon/ttt/TicTacToeTest.java
+++ src/test/java/com/jasonyoon/ttt/TicTacToeTest.java
@@ -1,5 +1,6 @@
 package com.jasonyoon.ttt;
 
+import org.junit.After;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -12,56 +13,66 @@ import static org.junit.Assert.assertEquals;
  */
 public class TicTacToeTest {
     TicTacToe ttt;
+    PrintStream stdout;
+    ByteArrayOutputStream consoleOutput;
 
     @Before
     public void setup() {
         ttt = new TicTacToe();
+        //redirect output so we can compare
+        stdout = System.out;
+        consoleOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(consoleOutput));
+    }
+
+    @After
+    public void cleanup() {
+        System.setOut(stdout);
     }
 
     @Test
-    public void runsUnitTest() {
-        //System.out.println("Running unit tests...");
-    }
-
-    @Test
-    public void displaysEmptyBoarD() {
-        System.out.println("displays an empty board");
+    public void displaysEmptyBoard() {
+        String expectedOutput = "|   |   |   |\n|   |   |   |\n|   |   |   |\n\n";
         ttt.displayBoard();
+        assertEquals(expectedOutput, consoleOutput.toString());
     }
 
-//    private String getActualOutput(TicTacToe t) {
-//        PrintStream stdout = System.out;
-//        ByteArrayOutputStream actualOutput = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(actualOutput));
-//        t.displayBoard();
-//        System.setOut(stdout);
-//        return actualOutput.toString();
-//    }
-
     @Test
-    public void displaySCompletedBoard() {
-        System.out.println("displays a completed board filled with X and O");
+    public void displaysCompletedBoard() {
+        String expectedOutput = "| X | O | X |\n| O | X | O |\n| X | O | X |\n\n";
         fillBoard();
         ttt.displayBoard();
+        assertEquals(expectedOutput, consoleOutput.toString());
     }
 
     private void fillBoard() {
-        ttt.makeMove('X', 1);
-        ttt.makeMove('O', 2);
-        ttt.makeMove('X', 3);
-        ttt.makeMove('O', 4);
-        ttt.makeMove('X', 5);
-        ttt.makeMove('O', 6);
-        ttt.makeMove('X', 7);
-        ttt.makeMove('O', 8);
-        ttt.makeMove('X', 9);
+        ttt.makeMove('X', 0, 0);
+        ttt.makeMove('O', 0, 1);
+        ttt.makeMove('X', 0, 2);
+        ttt.makeMove('O', 1, 0);
+        ttt.makeMove('X', 1, 1);
+        ttt.makeMove('O', 1, 2);
+        ttt.makeMove('X', 2, 0);
+        ttt.makeMove('O', 2, 1);
+        ttt.makeMove('X', 2, 2);
     }
 
     @Test
     public void playsAutomaticallyAlternating() {
-        System.out.println("plays a game automatically alternating between X and O");
         ttt.playGame();
+        String expectedOutput =
+                "| X |   |   |\n|   |   |   |\n|   |   |   |\n\n"
+                + "| X | O |   |\n|   |   |   |\n|   |   |   |\n\n"
+                + "| X | O | X |\n|   |   |   |\n|   |   |   |\n\n"
+                + "| X | O | X |\n| O |   |   |\n|   |   |   |\n\n"
+                + "| X | O | X |\n| O | X |   |\n|   |   |   |\n\n"
+                + "| X | O | X |\n| O | X | O |\n|   |   |   |\n\n"
+                + "| X | O | X |\n| O | X | O |\n| X |   |   |\n\n"
+                + "| X | O | X |\n| O | X | O |\n| X | O |   |\n\n"
+                + "| X | O | X |\n| O | X | O |\n| X | O | X |\n\n";
+        assertEquals(expectedOutput, consoleOutput.toString());
     }
+
 
 
 
